47840e7660244d1939075428a3bd576507691595
Juan C. Romualdo <jromualdo@razimports.com>
2020-01-06 13:55:28 -0600

    recent changes to django site

--- main/models.py
+++ main/models.py
@@ -5,6 +5,25 @@ class Tutorial(models.Model):
     tutorial_title = models.CharField(max_length=200)
     tutorial_content = models.TextField()
     tutorial_published = models.DateTimeField('date published', default=datetime.now)
+    #https://docs.djangoproject.com/en/2.1/ref/models/fields/#django.db.models.ForeignKey.on_delete
+
+
+    class Meta:
+        #Gives the proper plural name for admin
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.tutorial_title
+
+class TutorialSeries(models.Model):
+    tutorial_series = models.CharField(max_length=200)
+
+    tutorial_category = models.ForeignKey(TutorialCategory, default=1, verbose_name="Category", on_delete=models.SET_DEFAULT)
+    series_summary = models.CharField(max_length=200)
+
+    class Meta:
+        #otherwise we get "Tutorial Series in Admin"
+        verbose_name_plural = "Series"
+
+    def __str__(self):
+        return self.tutorial_series
--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from .models import Tutorial
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import logout, authenticate, login
 from django.contrib import messages
+from .forms import NewUserForm
 
 # Create your views here.
 def homepage(request):
@@ -15,15 +16,14 @@ def register(request):
     researcher = ''
     study = ''
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = NewUserForm(request.POST)
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f"New account created: {username}")
             login(request, user)
             return redirect("main:homepage")
-
-    #else: NEEDS TO BE FIXED
+    #else:
         #for msg in form.error_messages:
             #messages.error(request, f"{msg}: {form.error_messages[msg]}")
 
@@ -31,7 +31,7 @@ def register(request):
                       template_name = "main/register.html",
                       context={"form":form})
 
-    form = UserCreationForm
+    form = NewUserForm
     return render(request = request,
                   template_name = "main/register.html",
                   context={"form":form})
